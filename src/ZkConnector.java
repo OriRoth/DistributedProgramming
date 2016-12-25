e30a5d2b94a9f80f21df6eadb303d497741c7398
antonm94 <manton@student.ethz.ch>
2016-12-25 14:56:01 +0200

    ZkConnector Class

--- src/ZkConnector.java
+++ src/ZkConnector.java
@@ -1,3 +1,11 @@
+import java.io.IOException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.Watcher.Event.KeeperState;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.ZooKeeper.States;
+
+
 
 public class ZkConnector {
 
